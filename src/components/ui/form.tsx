21a6f2faa8edad89e97ff9a2df8ae0a2be896b05
Heron93 <mawandem98@gmail.com>
2026-07-09 23:39:14 +0200

    fixed errors

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,36 +1,19 @@
 import * as React from 'react'
-import {
-  Controller,
-  FieldPath,
-  FieldValues,
-  FormProvider,
-  type Control,
-  type ControllerRenderProps,
-  type UseFormReturn,
-} from 'react-hook-form'
+import { Controller, FieldPath, FieldValues, FormProvider, type Control, type ControllerRenderProps, type UseFormReturn } from 'react-hook-form'
 
-interface FormProps<TFieldValues extends FieldValues> {
-  form: UseFormReturn<TFieldValues>
-  children: React.ReactNode
-}
+interface FormProps<TFieldValues extends FieldValues> { form: UseFormReturn<TFieldValues>; children: React.ReactNode }
 
 function Form<TFieldValues extends FieldValues>({ form, children }: FormProps<TFieldValues>) {
   return <FormProvider {...form}>{children}</FormProvider>
 }
 
-interface FormFieldProps<
-  TFieldValues extends FieldValues,
-  TName extends FieldPath<TFieldValues>
-> {
+interface FormFieldProps<TFieldValues extends FieldValues, TName extends FieldPath<TFieldValues>> {
   control: Control<TFieldValues>
   name: TName
-  render: (props: { field: ControllerRenderProps<TFieldValues, TName> }) => React.ReactNode
+  render: (props: { field: ControllerRenderProps<TFieldValues, TName> }) => React.ReactElement
 }
 
-function FormField<
-  TFieldValues extends FieldValues,
-  TName extends FieldPath<TFieldValues>
->({ control, name, render }: FormFieldProps<TFieldValues, TName>) {
+function FormField<TFieldValues extends FieldValues, TName extends FieldPath<TFieldValues>>({ control, name, render }: FormFieldProps<TFieldValues, TName>) {
   return <Controller control={control} name={name} render={({ field }) => render({ field })} />
 }
 
